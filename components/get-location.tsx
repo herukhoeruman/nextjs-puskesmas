e22da3d1af0d1638b1bbae4b451c98396feac173
herukhoeruman <32447416+herukhoeruman@users.noreply.github.com>
2024-03-14 15:53:27 +0700

    feat: address

--- components/get-location.tsx
+++ components/get-location.tsx
@@ -1,11 +1,31 @@
 "use client";
+import axios from "axios";
 
 import { useEffect, useState } from "react";
 
 export const GetLocation: React.FC = () => {
   const [latitude, setLatitude] = useState<number | null>(null);
   const [longitude, setLongitude] = useState<number | null>(null);
+  const [address, setAddress] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
+
+  // useEffect(() => {
+  //   const getLocation = () => {
+  //     if (navigator.geolocation) {
+  //       navigator.geolocation.getCurrentPosition(
+  //         (position) => {
+  //           setLatitude(position.coords.latitude);
+  //           setLongitude(position.coords.longitude);
+  //         },
+  //         (err) => setError(err.message)
+  //       );
+  //     } else {
+  //       setError("Geolocation is not supported by this browser.");
+  //     }
+  //   };
+
+  //   getLocation();
+  // }, []);
 
   useEffect(() => {
     const getLocation = () => {
@@ -14,6 +34,7 @@ export const GetLocation: React.FC = () => {
           (position) => {
             setLatitude(position.coords.latitude);
             setLongitude(position.coords.longitude);
+            getAddress(position.coords.latitude, position.coords.longitude);
           },
           (err) => setError(err.message)
         );
@@ -25,11 +46,25 @@ export const GetLocation: React.FC = () => {
     getLocation();
   }, []);
 
+  const getAddress = async (lat: number, lng: number) => {
+    try {
+      const response = await axios.get(
+        `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json`
+      );
+      setAddress(response.data.display_name);
+    } catch (error) {
+      console.error("Error fetching address:", error);
+      setAddress("Address not found");
+    }
+  };
+
   return (
     <div>
       {latitude && longitude ? (
         <p>
           Latitude: {latitude}, Longitude: {longitude}
+          <br />
+          Address: {address}
         </p>
       ) : error ? (
         <p>Error: {error}</p>
