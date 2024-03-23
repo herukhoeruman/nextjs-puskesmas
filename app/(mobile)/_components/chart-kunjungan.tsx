d99e4b2d83aeb103755afff975f4f46bc0435e0a
herukhoeruman <32447416+herukhoeruman@users.noreply.github.com>
2024-03-23 16:40:36 +0700

    fix: 12 month

--- app/(mobile)/_components/chart-kunjungan.tsx
+++ app/(mobile)/_components/chart-kunjungan.tsx
@@ -29,14 +29,46 @@ const data = [
     name: "Apr",
     kunjungan: 212,
   },
+  {
+    name: "Mei",
+    kunjungan: 180,
+  },
+  {
+    name: "Jun",
+    kunjungan: 120,
+  },
+  {
+    name: "Jul",
+    kunjungan: 200,
+  },
+  {
+    name: "Agu",
+    kunjungan: 250,
+  },
+  {
+    name: "Sep",
+    kunjungan: 300,
+  },
+  {
+    name: "Okt",
+    kunjungan: 350,
+  },
+  {
+    name: "Nov",
+    kunjungan: 400,
+  },
+  {
+    name: "Des",
+    kunjungan: 450,
+  },
 ];
 
 export default function Example() {
   return (
-    <div className="h-60 m-4 border rounded-md shadow-sm p-2">
+    <div className="h-64 m-4 border rounded-md shadow-sm p-2">
       <ResponsiveContainer width="100%" height="100%">
         <BarChart width={500} height={300} data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
+          <CartesianGrid strokeDasharray="3 3" strokeOpacity="0" />
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
